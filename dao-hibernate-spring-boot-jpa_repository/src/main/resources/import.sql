b99feb0452a9d89d9dd5ea7c3b4bd08f8de2019b
Rodion Kadyumov <krrkes@gmail.com>
2023-04-23 07:25:50 +0500

    На проверку

---
+++ dao-hibernate-spring-boot-jpa_repository/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Rodion', 'Kaduymov', 36, +79000248128, 'Ekaterinburg');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Alina', 'Fausova', 21, +79824561895, 'Ekaterinburg');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Natalya', 'Fetisova', 35, +79000547625, 'Magnitogorsk');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Alia', 'Naimbanbaeva', 38, +794554278654, 'Magnitogorsk');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Anisa', 'Musina', 36, +79078954367 'Orenburg');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Fedor', 'Shtirkin', 34, +79123096478, 'Magnitogorsk');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Alexey', 'Popov', 48, null, 'Moscow');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES('Ekaterina', 'Uyrganova', 23, +79823057774, 'Magnitogorsk');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Tatyana', 'Pervuhina', 43, +79053536965, 'Kazan');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Olga', 'Upatovav', 31, +79205584318, 'Belgorod');
+INSERT INTO netology.person(first_name, last_name, age, phone_number, city_of_living) VALUES ('Albina', 'Urusova', 22, +79514825217, 'Ekaterinburg');
